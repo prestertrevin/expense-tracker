ee3f1eb7688de09879448d85d47a1d7b3d9f7c15
Trevin Prester <prestertrevin@gmail.com>
2026-01-15 11:39:47 -0700

    git init
    learning node.js
    + installed first dev dependency
    + created server.js, index.js, testfiles

---
+++ index.js
@@ -0,0 +1,9 @@
+import { getPosts } from './postController.js';
+
+console.log(getPosts());
+
+// const {generateRandomNumber, celciusToFahrenheit} = require('./utils');
+
+// console.log(`Random Number: ${generateRandomNumber()}`);
+
+// console.log(`Celcius to fahrenheit: ${celciusToFahrenheit(0)}`);
